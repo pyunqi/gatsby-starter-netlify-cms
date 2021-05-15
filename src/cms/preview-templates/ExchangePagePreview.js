f6acd8c061e35480be3d8ef60e3b132f5b09750d
pyunqi <pyunqi@gmail.com>
2021-05-15 22:07:59 +0800

    change product to exchange

---
+++ src/cms/preview-templates/ExchangePagePreview.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { ExchangePageTemplate } from '../../templates/about-page'
+
+const ExchangePagePreview = ({ entry, widgetFor }) => (
+  <ExchangePageTemplate
+    title={entry.getIn(['data', 'title'])}
+    content={widgetFor('body')}
+  />
+)
+
+ExchangePagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  widgetFor: PropTypes.func,
+}
+
+export default ExchangePagePreview
---
+++ src/pages/exchange/index.md
@@ -0,0 +1,12 @@
+---
+templateKey: exchange-page
+path: /exchange
+title: 傻币
+---
+# 重要的事情说三遍！
+
+### 我们是BSC上的真 SHAB 其他都不是！
+
+### 我们是BSC上的真 SHAB 其他都不是！
+
+### 我们是BSC上的真 SHAB 其他都不是！
