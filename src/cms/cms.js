d50db24ea3a1eb018e9ba38200478978fcf67320
pyunqi <pyunqi@gmail.com>
2021-05-16 12:16:01 +0800

    update en mian and exchange page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,15 +2,17 @@ import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
+import ENIndexPagePreview from './preview-templates/ENIndexPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ExchangePagePreview from './preview-templates/ExchangePagePreview'
+import ENExchangePagePreview from './preview-templates/ENExchangePagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('ENIndex', ENIndexPagePreview)
 CMS.registerPreviewTemplate('exchange', ExchangePagePreview)
+CMS.registerPreviewTemplate('enexchange', ENExchangePagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
